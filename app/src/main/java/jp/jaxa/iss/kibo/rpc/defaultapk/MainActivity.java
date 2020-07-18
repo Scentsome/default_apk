1a656b41b98afa9dd41489799822b910a13c26ce
Philip <Philipkd1112@gmail.com>
2020-07-18 11:54:31 +0800

    Test

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/MainActivity.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/MainActivity.java
@@ -1,14 +1,126 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.opencv.android.InstallCallbackInterface;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.aruco.Aruco;
+import org.opencv.aruco.Dictionary;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
 //Philip
+ImageView imageView;
+    TextView textView;
+    LoaderCallbackInterface loaderCalllbackInterface = new LoaderCallbackInterface(){
+        @Override
+        public void onManagerConnected(int status){
+
+        }
+        @Override
+        public void onPackageInstall(int operation, InstallCallbackInterface callback){
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    imageView = findViewById(R.id.imageView);
+    textView = findViewById(R.id.textView);
+        if(!OpenCVLoader.initDebug()){
+        boolean success = OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0 ,this, loaderCalllbackInterface);
+    } else{
+        loaderCalllbackInterface.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+    }
+    test();
+}
+class DownloadImagaTask extends AsyncTask<String, Void, Bitmap> {
+    @Override
+    protected Bitmap doInBackground(String... urls) {
+        String url = urls[0];
+        Bitmap bitmap = null;
+        try {
+            InputStream inputStream = new java.net.URL(url).openStream();
+            bitmap = BitmapFactory.decodeStream(inputStream);
+            final Bitmap finalBitmap = bitmap;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    imageView.setImageBitmap(finalBitmap);
+                }
+            });
+            return bitmap;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+        decodeARTag(bitmap);
+    }
+    //void decodeQRCode(Bitmap bimap){
+    // Mat mat = new Mat();
+    //  Utils.bitmapToMat(bitmap,mat);
+    //  QRCodeDetector qrCodeDetector = new QRCodeDetector();
+    //   String result = qrCodeDetector.detectAndDecode(mat);
+    //   textView.setText(result);
+
+}
+    void decodeARTag(Bitmap bitmap){
+        Mat mat = new Mat();
+        Mat ids = new Mat();
+        ArrayList<Mat> corners = new ArrayList<>();
+        Utils.bitmapToMat(bitmap, mat);
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGBA2BGR);
+        Dictionary dictionary = Dictionary.get(Aruco.DICT_6X6_250);
+        Aruco.detectMarkers(mat,dictionary,corners,ids);
+        String idlist = "";
+        for(int i = 0; i<6; i++){
+            int id = (int)(ids.get(i,0)[0]);
+            idlist += id+",";
+        }
+        textView.setText(idlist);
+
+
+        //int id1 = (int)(ids.get(0,0)[0]);
+        //int id2 = (int)(ids.get(1,0)[0]);
+        //int id3 = (int)(ids.get(2,0)[0]);
+        //int id4 = (int)(ids.get(3,0)[0]);
+        //int id5 = (int)(ids.get(4,0)[0]);
+        //textView.setText(id1+","+id2+","+id3+","+id4+","+id5);
+        Aruco.drawDetectedMarkers(mat,corners,ids);
+        Bitmap resultbitmap = Bitmap.createBitmap(mat.cols(),mat.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(mat,resultbitmap);
+        imageView.setImageBitmap((resultbitmap));
+    }
+
+
+
+    void test(){
+        DownloadImagaTask task = new DownloadImagaTask();
+        task.execute("https://docs.opencv.org/trunk/singlemarkersoriginal.jpg");
+
     }
 }
+
 
